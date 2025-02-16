6aa9813d17e23004a0a022f6f2b4f88b90fa18f2
daniello7 <daniellopezolmos7@gmail.com>
2025-02-16 16:22:19 +0100

    Paso 1, Creo Count.vue

---
+++ src/components/Count.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Hola</h1>
+  <p>
+    {{ count }}
+  </p>
+  <button @click="increment">Incrementar</button>
+  <button @click="decrement">Decrementar</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+const increment = () => count.value++
+const decrement = () => count.value--
+</script>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
